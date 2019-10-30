fd1721f2b20d3704593a2a63e2a25a289c367190
Alexander Leong <alexleong7@gmail.com>
2019-10-30 16:33:19 -0700

    integrate axios

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     lettuce: 0.5,
@@ -82,7 +83,24 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You just continued...');
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice, // DON'T DO THIS IN PRODUCTION, calculate price server-side
+            customer: {
+                name: 'Alex Leong',
+                address: {
+                    street: 'Street St',
+                    zipCode: '12345',
+                    country: 'US'
+                },
+                email: 'test@test.com'
+            },
+            deliveryMethod: 'fastest'
+
+        }
+        axios.post('/orders.json', order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
     }
 
     render() {
